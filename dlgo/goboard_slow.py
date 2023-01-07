3794e921d83203d73e3ee2413cae17f1ff23f4d9
suyang <2506806016@qq.com>
2023-01-07 19:41:55 +0800

    building: 眼的判断

--- dlgo/goboard_slow.py
+++ dlgo/goboard_slow.py
@@ -10,8 +10,16 @@ from dlgo.gotypes import Player
 
 
 class Move:
+    """
+    动作：
+    - 落子 play
+    - 跳过 pass
+    - 认输 resign
+    """
+
     def __int__(self, point=None, is_pass=False, is_resign=False):
         assert (point is not None) ^ is_pass ^ is_resign  # 至少采取落子、跳过、认输的任意一个动作
+        self.point = point
         self.is_play = (point is not None)
         self.is_pass = is_pass
         self.is_resign = is_resign
@@ -29,8 +37,11 @@ class Move:
         return Move(is_resign=True)
 
 
-# 棋链
 class GoString:
+    """
+    棋链: 颜色相同且连续的棋子构成的集合
+    """
+
     def __init__(self, color, stones, liberties):
         """
         :param color: 颜色
@@ -70,12 +81,22 @@ class GoString:
 
 
 class Board:
+    """
+    棋盘
+    """
+
     def __init__(self, num_rows, num_cols):
         self.num_rows = num_rows
         self.num_cols = num_cols
         self._grid = {}  # 存储棋链的字典 key: Point value: GoString
 
     def place_stone(self, player, point):
+        """
+        处理落子的相关动作，包括
+        - 创建合并棋链
+        - 更新气数
+        - 提子
+        """
         assert self.is_on_grid(point)
         assert self._grid.get(point) is None
         adjacent_same_color = []
@@ -109,6 +130,10 @@ class Board:
                 self._remove_string(other_color_string)
 
     def is_on_grid(self, point):
+        """
+        判断Point是否在棋盘上
+        """
+
         return 1 <= point.row <= self.num_rows and \
             1 <= point.col <= self.num_cols
 
@@ -122,12 +147,20 @@ class Board:
         return string.color
 
     def get_go_string(self, point):
+        """
+        获取Point所在棋链
+        """
+
         string = self._grid.get(point)
         if string is None:
             return None
         return string
 
     def _remove_string(self, string):
+        """
+        移除棋链即提子
+        """
+
         # 一个一个的提走棋子
         for point in string:
             # 为相邻的棋链增加气数
@@ -138,3 +171,125 @@ class Board:
                 if neighbor_string is not string:  # 保证不是自己
                     neighbor_string.add_libertiy(point)
             self._grid[point] = None
+
+
+class GameState:
+    """
+    跟踪游戏状态，最终形成一个动作序列
+    """
+
+    def __init__(self, board, next_player, previous, move):
+        """
+        :param board: Board
+        :param next_player: Player 下一位下棋的人
+        :param previous: GameState 上一轮的游戏状态
+        :param move: Move 上一步动作
+        """
+
+        self.board = board
+        self.next_player = next_player
+        self.previous_state = previous
+        self.last_move = move
+
+    def apply_move(self, move):
+        """
+        添加新的动作，返回新的状态
+        :param move: Move
+        :return GameState
+        """
+
+        if move.is_play:
+            # 深拷贝，保存每一步的棋盘状态，这也是为什么叫goboard_slow的原因，但是实现比较简单
+            next_board = copy.deepcopy(self.board)
+            next_board.place_stone(self.next_player, move.point)
+        else:
+            next_board = self.board
+        return GameState(next_board, self.next_player.other, self, move)
+
+    @classmethod
+    def new_game(cls, board_size):
+        """
+        创建新游戏
+        :param board_size: int or tuple 棋盘大小
+        :return GameState
+        """
+
+        if isinstance(board_size, int):
+            board_size = (board_size, board_size)
+        board = Board(*board_size)
+        return GameState(board, Player.black, None, None)
+
+    def is_over(self):
+        """
+        判断游戏结束
+        :return: bool
+        """
+
+        if self.last_move is None:
+            return False
+        if self.last_move.is_resgin:
+            return True
+        # 判断倒数第二手
+        second_last_move = self.previous_state.last_move
+        if second_last_move is None:
+            return False
+        # 双方都选择停一手时游戏结束
+        return self.last_move.is_pass and second_last_move.is_pass
+
+    def is_move_self_capture(self, player, move):
+        """
+        检查自吃
+        :param player: Player
+        :param move: Move
+        :return: bool
+        """
+
+        if not move.is_play:
+            return False
+        next_board = copy.deepcopy(self.board)
+        next_board.place_stone(player, move.point)
+        new_string = next_board.get_go_string(move.point)
+        # 如果新的棋链的气为0，则发生了自吃
+        return new_string.num_liberties == 0
+
+    @property
+    def situation(self):
+        return self.next_player, self.board
+
+
+    def does_move_violate_ko(self, player, move):
+        """
+        检查是否违反劫争规则
+        :param player: Player
+        :param move: Move
+        :return: bool
+        """
+
+        if not move.is_play:
+            return False
+        next_board = copy.deepcopy(self.board)
+        next_board.place_stone(player, move.point)
+        next_situation = (player, next_board)
+        past_state = self.previous_state
+        while past_state is not None:
+            if past_state.situation == next_situation:
+                return True
+            past_state = past_state.previous_state
+        return False
+
+    def is_valid_move(self, move):
+        """
+        检查动作的合法性
+        :param move: Move
+        :return: bool
+        """
+
+        if self.is_over():
+            return False
+        if move.is_pass or move.is_resign:
+            return True
+        return (
+            self.board.get(move.point) is None and
+            not self.is_move_self_capture(self.next_player, move) and
+            not self.does_move_violate_ko(self.next_player, move))
+
